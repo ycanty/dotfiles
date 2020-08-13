84a4b1869b31a4b02af8dcd9bdd90239a03d2642
Yves Canty <ycanty@coveo.com>
2020-08-13 09:44:52 -0400

    work-specific: add aws ecr stuff

--- configs/work-specific/work-specific.zsh
+++ configs/work-specific/work-specific.zsh
@@ -1,1 +1,16 @@
 source ~/.zsh/work-plugins.zsh
+
+# Required by tools that use the AWS SDK so they can load the config from ~/.aws/config
+# See: https://github.com/awslabs/amazon-ecr-credential-helper/issues/63
+export AWS_SDK_LOAD_CONFIG=true
+
+function docker-ecr-login() {
+  local account_id
+  local region
+  local domain
+  account_id=$(aws configure get role_arn | sed -Ee 's/^.*::([0-9]+):.*$/\1/')
+  region=$(aws configure get region)
+  domain="${account_id}.dkr.ecr.${region}.amazonaws.com"
+  echo "Logging in to ${domain}"
+  aws ecr get-login-password | docker login --username AWS --password-stdin "${domain}"
+}
